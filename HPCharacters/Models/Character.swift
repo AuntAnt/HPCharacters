a4544d42cb163e493495c4861a5d352dddf5637d
Anton Kuzmin <furthertwoant@gmail.com>
2023-03-27 19:48:01 +0400

    Implemented networking with alamofire

--- HPCharacters/Models/Character.swift
+++ HPCharacters/Models/Character.swift
@@ -14,6 +14,20 @@ struct Character: Decodable {
     let house: String
     let yearOfBirth: Int?
     let image: String
+    
+    init(from data: [String: Any]) {
+        name = data["name"] as? String ?? ""
+        actorWhoPlayed = data["actor"] as? String ?? ""
+        alternateNames = data["alternate_names"] as? [String] ?? []
+        house = data["house"] as? String ?? ""
+        yearOfBirth = data["yearOfBirth"] as? Int
+        image = data["image"] as? String ?? ""
+    }
+    
+    static func getAllCharacters(from data: Any) -> [Character] {
+        guard let loadedCharacters = data as? [[String: Any]] else { return [] }
+        return loadedCharacters.map { Character(from: $0) }
+    }
 }
 
 extension Character {
--- HPCharacters/Networking/Network.swift
+++ HPCharacters/Networking/Network.swift
@@ -6,67 +6,40 @@
 //
 
 import Foundation
+import Alamofire
 
 final class NetworkManager {
     static let shared = NetworkManager()
     
     private init() {}
     
-    /// Just for practice, currently not needed in this app
-    func fetch<T: Decodable>(to type: T.Type, from url: URL, completion: @escaping (Result<T, NetworkError>) -> Void) {
-        URLSession.shared.dataTask(with: url) { data, _, error in
-            guard let data else {
-                completion(.failure(.noData))
-                print(error?.localizedDescription ?? "No error description")
-                return
-            }
-            
-            let decoder = JSONDecoder()
-            
-            do {
-                let dataModel = try decoder.decode(T.self, from: data)
-                DispatchQueue.main.async {
-                    completion(.success(dataModel))
-                }
-            } catch {
-                completion(.failure(.decodingError))
-            }
-        }.resume()
-    }
-    
     ///Getting characters
-    func fetchCharacters(from url: URL, completion: @escaping(Result<[Character], NetworkError>) -> Void) {
-        URLSession.shared.dataTask(with: url) { data, _, error in
-            guard let data else {
-                completion(.failure(.noData))
-                print(error?.localizedDescription ?? "No error description")
-                return
-            }
-            
-            let decoder = JSONDecoder()
-            
-            do {
-                let students = try decoder.decode([Character].self, from: data)
-                DispatchQueue.main.async {
-                    completion(.success(students))
+    func fetchCharacters(from url: URL, completion: @escaping(Result<[Character], AFError>) -> Void) {
+        AF.request(url)
+            .validate()
+            .responseJSON { response in
+                switch response.result {
+                case .success(let value):
+                    let characters = Character.getAllCharacters(from: value)
+                    completion(.success(characters))
+                case .failure(let error):
+                    completion(.failure(error))
                 }
-            } catch {
-                completion(.failure(.decodingError))
             }
-        }.resume()
     }
     
     ///Getting image
-    func fetchImage(from url: URL, completion: @escaping(Result<Data, NetworkError>) -> Void) {
-        DispatchQueue.global().async {
-            guard let imageData = try? Data(contentsOf: url) else {
-                completion(.failure(.noData))
-                return
+    func fetchImage(from url: URL, completion: @escaping(Result<Data, AFError>) -> Void) {
+        AF.request(url)
+            .validate()
+            .responseData { dataResponse in
+                switch dataResponse.result {
+                case .success(let imageData):
+                    completion(.success(imageData))
+                case .failure(let error):
+                    completion(.failure(error))
+                }
             }
-            DispatchQueue.main.async {
-                completion(.success(imageData))
-            }
-        }
     }
 }
 
@@ -82,9 +55,4 @@ enum Link {
             return URL(string: "https://hp-api.onrender.com/api/characters/students")!
         }
     }
-}
-
-enum NetworkError: Error {
-    case noData
-    case decodingError
 }
